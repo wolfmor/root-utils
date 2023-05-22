5014b9214c40c83acccaaf51eae861decf0cfce9
Moritz Wolf <moritz.wolf@desy.de>
2023-05-22 21:47:27 +0200

    improve examples

--- plotExample_events.py
+++ plotExample_events.py
@@ -2,52 +2,53 @@ import ROOT
 from utils import PlotFactory, HistoSample, TreeSample, Variable,  Ratio
 
 pf = PlotFactory(
-    # outputpath='.',  # change this to a path that suits you
-    outputpath='/afs/desy.de/user/w/wolfmor/Plots/SoftDisplacedTrack/Stop',  # change this to a path that suits you
+    outputpath='.',  # change this to a path that suits you
     outputpattern='compareStopDirect_VARIABLE',  # change this to a name that suits you (VARIABLE will be replaced by the variable name)
     outputformat='pdf',
 
-    normalize=True,
+    normalize=True,  # for now, we only want to look at differences in shape
     ylabel='Fraction of Events',
     ylabelratio='Stop/Direct',
 
     yaxisrangeratio=(0.0001, 2.9999),
     ratiohlines=[1],
 
-    text='',
+    text='2016',
 )
 
-path = '/pnfs/desy.de/cms/tier2/store/user/mowolf/NTuples/NTuplesV9/'
-eventselection = None
+path = '/pnfs/desy.de/cms/tier2/store/user/mowolf/NTuples/NTuplesV10/'  # the path where the ntuples are stored
+eventselection = None  # don't apply any additional event selection cuts
+
+ntestfiles = 0  # if it takes too long to process all files, use only a few to test
 
 pf.add_samples([
     # add two direct higgsino production samples
-    TreeSample(category='line', name='direct_mChi115_dm0p57',
-               tree='tEvent', files=path + 'SignalV2_16/higgsino94x_susyall_mChipm115GeV_dm0p57GeV_pu35_part*_NTuple_job*.root',
+    TreeSample(ntestfiles=ntestfiles, category='line', name='direct_mChi115_dm0p57',
+               tree='tEvent', files=path + 'SignalV4_16/step3_higgsinoDm0eDmpm_RunIISpring21UL16FS_susyall_mChipm115GeV_dm0p568GeV_part*_NTuple_job*.root',
                eventselection=eventselection,
                color=ROOT.kAzure+1),
-    TreeSample(category='line', name='direct_mChi115_dm0p97',
-               tree='tEvent', files=path + 'SignalV2_16/higgsino94x_susyall_mChipm115GeV_dm0p97GeV_pu35_part*_NTuple_job*.root',
+    TreeSample(ntestfiles=ntestfiles, category='line', name='direct_mChi115_dm0p97',
+               tree='tEvent', files=path + 'SignalV4_16/step3_higgsinoDm0eDmpm_RunIISpring21UL16FS_susyall_mChipm115GeV_dm0p968GeV_part*_NTuple_job*.root',
                eventselection=eventselection,
                color=ROOT.kRed),
 
     # add the corresponding stop production samples
-    TreeSample(category='marker', name='stop_mStop800_mChi115_dm0p6',
+    TreeSample(ntestfiles=ntestfiles, category='marker', name='stop_mStop800_mChi115_dm0p6',
                tree='tEvent', files=path + 'SignalStopV3_16/higgsino_Summer16_stopstop_800GeV_mChipm115GeV_dm0p6GeV_pu35_part*_NTuple_job*.root',
                eventselection=eventselection,
                color=ROOT.kAzure+1),
-    TreeSample(category='marker', name='stop_mStop800_mChi115_dm1p0',
+    TreeSample(ntestfiles=ntestfiles, category='marker', name='stop_mStop800_mChi115_dm1p0',
                tree='tEvent', files=path + 'SignalStopV3_16/higgsino_Summer16_stopstop_800GeV_mChipm115GeV_dm1p0GeV_pu35_part*_NTuple_job*.root',
                eventselection=eventselection,
                color=ROOT.kRed),
 ])
 
 pf.add_variables([
-    # first some generator information
+    # first some GEN information
     Variable(name='n_chiC1', axisrange=(0, 3), nbins=3),
     Variable(name='n_chiN2', axisrange=(0, 3), nbins=3),
     Variable(name='chiC1_log10_decaylengthXY_', axisrange=(-5, 1), nbins=60),
-    Variable(name='chiC1_log10_chidecaylengthZ_', axisrange=(-5, 1), nbins=60),  # sorry for the inconsistent variable name...
+    Variable(name='chiC1_log10_chidecaylengthZ_', axisrange=(-5, 1), nbins=60),
     Variable(name='chiC1_pionPt', axisrange=(-1, 9), nbins=100),
     Variable(name='chiC1_hasMatchedTrackPion', axisrange=(0, 2), nbins=2),
 
--- plotExample_tracks.py
+++ plotExample_tracks.py
@@ -2,45 +2,57 @@ import ROOT
 from utils import PlotFactory, HistoSample, TreeSample, Variable,  Ratio
 
 pf = PlotFactory(
-    # outputpath='.',  # change this to a path that suits you
-    outputpath='/afs/desy.de/user/w/wolfmor/Plots/SoftDisplacedTrack/Stop',  # change this to a path that suits you
+    outputpath='.',  # change this to a path that suits you
     outputpattern='compareStopDirect_VARIABLE',  # change this to a name that suits you (VARIABLE will be replaced by the variable name)
     outputformat='pdf',
 
-    normalize=True,
+    normalize=True,  # for now, we only want to look at differences in shape
     ylabel='Fraction of Tracks',
     ylabelratio='Stop/Direct',
 
     yaxisrangeratio=(0.0001, 2.9999),
     ratiohlines=[1],
 
-    text='Signal tracks matched to pion',
+    text='2016',
 )
 
-path = '/pnfs/desy.de/cms/tier2/store/user/mowolf/NTuples/NTuplesV9/'
-eventselection = None
+path = '/pnfs/desy.de/cms/tier2/store/user/mowolf/NTuples/NTuplesV10/'  # the path where the ntuples are stored
+eventselection = None  # don't apply any additional event selection cuts
 trackselection = 'track_isSignalTrack==1'  # we only want to look at our signal track
+
+ntestfiles = 0  # if it takes too long to process all files, use only a few to test
 
 pf.add_samples([
     # add two direct higgsino production samples
-    TreeSample(category='line', name='direct_mChi115_dm0p57',
-               tree='tEvent', files=path + 'SignalV2_16/higgsino94x_susyall_mChipm115GeV_dm0p57GeV_pu35_part*_NTuple_job*.root',
+    TreeSample(ntestfiles=ntestfiles, category='line', name='direct_mChi115_dm0p57',
+               tree='tEvent', files=path + 'SignalV4_16/step3_higgsinoDm0eDmpm_RunIISpring21UL16FS_susyall_mChipm115GeV_dm0p568GeV_part*_NTuple_job*.root',
                eventselection=eventselection, vectorselection=trackselection,
                color=ROOT.kAzure+1),
-    TreeSample(category='line', name='direct_mChi115_dm0p97',
-               tree='tEvent', files=path + 'SignalV2_16/higgsino94x_susyall_mChipm115GeV_dm0p97GeV_pu35_part*_NTuple_job*.root',
+    TreeSample(ntestfiles=ntestfiles, category='line', name='direct_mChi115_dm0p97',
+               tree='tEvent', files=path + 'SignalV4_16/step3_higgsinoDm0eDmpm_RunIISpring21UL16FS_susyall_mChipm115GeV_dm0p968GeV_part*_NTuple_job*.root',
                eventselection=eventselection, vectorselection=trackselection,
                color=ROOT.kRed),
 
     # add the corresponding stop production samples
-    TreeSample(category='marker', name='stop_mStop800_mChi115_dm0p6',
+    TreeSample(ntestfiles=ntestfiles, category='marker', name='stop_mStop800_mChi115_dm0p6',
                tree='tEvent', files=path + 'SignalStopV3_16/higgsino_Summer16_stopstop_800GeV_mChipm115GeV_dm0p6GeV_pu35_part*_NTuple_job*.root',
                eventselection=eventselection, vectorselection=trackselection,
                color=ROOT.kAzure+1),
-    TreeSample(category='marker', name='stop_mStop800_mChi115_dm1p0',
+    TreeSample(ntestfiles=ntestfiles, category='marker', name='stop_mStop800_mChi115_dm1p0',
                tree='tEvent', files=path + 'SignalStopV3_16/higgsino_Summer16_stopstop_800GeV_mChipm115GeV_dm1p0GeV_pu35_part*_NTuple_job*.root',
                eventselection=eventselection, vectorselection=trackselection,
                color=ROOT.kRed),
+
+    # to compare the signal tracks with all other tracks in the event (note the different "vectorselection")
+    TreeSample(ntestfiles=ntestfiles, category='line', name='direct_insignalBkg',
+               tree='tEvent', files=path + 'SignalV4_16/step3_higgsinoDm0eDmpm_RunIISpring21UL16FS_susyall_mChipm*GeV_dm*GeV_part1of25_NTuple_job*.root',  # it's enough to only use the *part1* file of each model point
+               eventselection=eventselection, vectorselection='track_isSignalTrack==0&&track_isSusyTrack==0',
+               color=ROOT.kBlack),
+
+    TreeSample(ntestfiles=ntestfiles, category='marker', name='stop_insignalBkg',
+               tree='tEvent', files=path + 'SignalStopV3_16/higgsino_Summer16_stopstop_*GeV_mChipm*GeV_dm*GeV_pu35_part1of25_NTuple_job*.root',  # it's enough to only use the *part1* file of each model point
+               eventselection=eventselection, vectorselection='track_isSignalTrack==0&&track_isSusyTrack==0',
+               color=ROOT.kBlack),
 ])
 
 pf.add_variables([
@@ -49,6 +61,8 @@ pf.add_variables([
     Variable(name='track_log10_dxy_', axisrange=(-5, 1), nbins=60),
     Variable(name='track_log10_dz_', axisrange=(-5, 1), nbins=60),
     Variable(name='track_dphiMet', axisrange=(-3.5, 3.5), nbins=70),
+
+    Variable(name='track_drminJet30', axisrange=(0., 10), nbins=50),
 
     # add more! e.g. isolation-related variables, impact parameter (IP),...
 ])
